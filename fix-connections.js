27639b3c300d7469a3f1c6887d74a1a8d6f1c509
Robert Thiel <robert@devheadllc.com>
2025-06-17 05:47:01 -0700

    Resetting Changes

--- fix-connections.js
+++
@@ -1,7 +0,0 @@
-                { from: 'step-27', to: 'step-28', label: 'Next', connectionClass: 'step-connection' },
-                { from: 'step-28', to: 'doc-feature-registry', label: 'Update', connectionClass: 'step-to-doc-connection' },
-                { from: 'step-28', to: 'step-29', label: 'Next', connectionClass: 'step-connection' },
-                { from: 'step-29', to: 'step-30', label: 'Next', connectionClass: 'step-connection' },
-                { from: 'step-30', to: 'step-1', label: 'Restart process', connectionClass: 'step-connection' },
-                
-                // Document relationships 
