ba40ec0a9f92f8c9be1cebcd8731704340a6e21b
Robert Thiel <robert@devheadllc.com>
2025-06-19 14:40:49 -0700

    Removing legacy code

--- mobile/src/components/battle/NetworkLines.tsx
+++ mobile/src/components/battle/NetworkLines.tsx
@@ -1,9 +1,8 @@
-import React, { useMemo, useEffect, useRef } from 'react';
+import React, { useMemo } from 'react';
 import { StyleSheet } from 'react-native';
 import Svg, { Line } from 'react-native-svg';
 import { COLORS } from '../../styles/theme';
 import { DataStream } from './DataStream';
-import { BattlePerformanceMonitor } from '../../battle/core/BattlePerformanceMonitor';
 
 const NETWORK_CONNECTIONS = [
   [0, 3], [3, 6], [1, 4], [4, 7], [2, 5], [5, 8], // Horizontal
@@ -19,13 +18,7 @@ type Props = {
 };
 
 export const NetworkLines = React.memo(({ nodes, width, height }: Props) => {
-  const performanceMonitor = BattlePerformanceMonitor.getInstance();
-  const frameIdRef = useRef<number>();
-  const renderStartTimeRef = useRef(0);
-
   const { lines, dataStreams } = useMemo(() => {
-    const startTime = performance.now();
-    
     const lineElements = NETWORK_CONNECTIONS.map(([from, to], index) => (
       <Line
         key={index}
@@ -50,32 +43,8 @@ export const NetworkLines = React.memo(({ nodes, width, height }: Props) => {
       />
     ));
 
-    performanceMonitor.recordJSThreadUsage((performance.now() - startTime) / 16.67);
     return { lines: lineElements, dataStreams: streamElements };
   }, [nodes]);
-
-  useEffect(() => {
-    const renderTime = performance.now() - renderStartTimeRef.current;
-    performanceMonitor.recordJSThreadUsage(renderTime / 16.67);
-  });
-
-  useEffect(() => {
-    let frameId: number;
-    const monitorFrame = () => {
-      performanceMonitor.recordFrame();
-      frameId = requestAnimationFrame(monitorFrame);
-    };
-    
-    frameId = requestAnimationFrame(monitorFrame);
-    frameIdRef.current = frameId;
-
-    return () => {
-      cancelAnimationFrame(frameId);
-      frameIdRef.current = undefined;
-    };
-  }, []);
-
-  renderStartTimeRef.current = performance.now();
 
   return (
     <Svg style={StyleSheet.absoluteFill}>
