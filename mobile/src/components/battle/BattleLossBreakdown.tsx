7573deb739b6cdeed1a909b4b2566c545c40624f
devheadllc <robert@devheadllc.com>
2025-12-19 17:54:12 -0700

    Update BattleLossBreakdown.tsx

--- mobile/src/components/battle/BattleLossBreakdown.tsx
+++ mobile/src/components/battle/BattleLossBreakdown.tsx
@@ -27,8 +27,8 @@ export const BattleLossBreakdown: React.FC<Props> = ({ battleEndData }) => {
     }, 0);
   };
 
-  const attackerBotsDestroyed = calculateBotQuantityLosses(enemyBattalions);
-  const defenderBotsLost = calculateBotQuantityLosses(enemyBattalions);
+  const attackerBotsDestroyed = calculateBotQuantityLosses(userBattalions);
+  const defenderBotsLost = calculateBotQuantityLosses(userBattalions);
   const isUserVsUser = isUserDefender === true;
 
   return (
