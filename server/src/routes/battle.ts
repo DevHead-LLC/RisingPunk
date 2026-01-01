56bde25177dfa712de429953a6103fe02b9d28dd
devheadllc <robert@devheadllc.com>
2026-01-01 11:14:10 -0700

    Update battle.ts

--- server/src/routes/battle.ts
+++ server/src/routes/battle.ts
@@ -45,31 +45,35 @@ router.post<{}, BattleResponse, StartBattleRequest['body']>(
       
       const battle = await battleController.startBattle(req.user._id, defenderId || 'computer', screenWidth, screenHeight, userBattalions, defenderNpcSlug, unlockHackRigOnWin === true, defenderNpcInstanceId);
       
-      const battleDoc = await Battle.findOne({ battleId: battle.battleId });
-      const actualDefenderNpcSlug = (battleDoc as any)?.defenderNpcSlug;
-      
-      if (actualDefenderNpcSlug) {
-        const npc = await NPCService.getNPCBySlug(actualDefenderNpcSlug);
-        if (npc && npc.userLevelAssociation === 1) {
-          const existingProgress = await UserTaskProgress.findOne({ userId: req.user._id });
-          const wasAlreadyAttacked = existingProgress?.attackedLevel1NpcAt;
-          
-          if (!wasAlreadyAttacked) {
-            await UserTaskProgress.findOneAndUpdate(
-              { userId: req.user._id },
-              {
-                $set: { attackedLevel1NpcAt: new Date() },
-                $setOnInsert: {
-                  completedTasks: [],
-                  collectedTasks: [],
-                  skippedTasks: [],
-                  showTaskGuide: true
-                }
-              },
-              { upsert: true, new: true }
-            );
+      try {
+        const battleDoc = await Battle.findOne({ battleId: battle.battleId });
+        const actualDefenderNpcSlug = (battleDoc as any)?.defenderNpcSlug;
+        
+        if (actualDefenderNpcSlug) {
+          const npc = await NPCService.getNPCBySlug(actualDefenderNpcSlug);
+          if (npc && npc.userLevelAssociation === 1) {
+            const existingProgress = await UserTaskProgress.findOne({ userId: req.user._id });
+            const wasAlreadyAttacked = existingProgress?.attackedLevel1NpcAt;
+            
+            if (!wasAlreadyAttacked) {
+              await UserTaskProgress.findOneAndUpdate(
+                { userId: req.user._id },
+                {
+                  $set: { attackedLevel1NpcAt: new Date() },
+                  $setOnInsert: {
+                    completedTasks: [],
+                    collectedTasks: [],
+                    skippedTasks: [],
+                    showTaskGuide: true
+                  }
+                },
+                { upsert: true, new: true }
+              );
+            }
           }
         }
+      } catch (taskTrackingError) {
+        console.error('Error tracking Level 1 NPC attack for task guide:', taskTrackingError);
       }
       
       res.status(201).json({ battleId: battle.battleId });
