1f5e31efcc398da9ef9ebaf0ccbc5cdb83039ccc
devheadllc <robert@devheadllc.com>
2026-03-03 16:16:25 -0700

    Add images, adjust image asset for research

--- mobile/src/components/research/FeatureCard.tsx
+++ mobile/src/components/research/FeatureCard.tsx
@@ -75,7 +75,7 @@ const BACKGROUND_IMAGE_MAP: Record<string, ImageSourcePropType> = {
   'reduce-insurance-01': require('../../assets/images/researchCenter/decreaseInsurance.png'),
   'reduce-insurance-02': require('../../assets/images/researchCenter/decreaseInsurance.png'),
   'reduce-insurance-03': require('../../assets/images/researchCenter/decreaseInsurance.png'),
-  'reduce-rent-mortgage-05': require('../../assets/images/researchCenter/decreaseInsurance.png'),
+  'reduce-rent-mortgage-05': require('../../assets/images/researchCenter/mortgageReduction.png'),
   'reduce-tax-expense-02': require('../../assets/images/researchCenter/taxExpenseReduction.png'),
   'probe': require('../../assets/images/researchCenter/probeResearch.png'),
   'mark-2-bots': require('../../assets/images/researchCenter/botUpgradeEvolution.png'),
